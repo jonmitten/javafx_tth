fef88d384e4600864de3efef72851a75ef06c489
jonmitten <jonmitten@gmail.com>
2017-02-08 13:07:20 -0800

    adds interactivity via textfield

--- sup/src/sample/Main.java
+++ sup/src/sample/Main.java
@@ -5,6 +5,9 @@ import javafx.application.Application;
 import javafx.scene.Group;
 // import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -18,8 +21,14 @@ public class Main extends Application {
         // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Group root = new Group();
         Text txt = new Text("Sup?");
+        TextField nameFld = new TextField();
+        Button btn = new Button();
+        btn.setText("Say S'up");
+        btn.setOnAction(evt -> System.out.printf("Sup, %s!%n", nameFld.getText()));
         txt.setY(60);
-        root.getChildren().add(txt);
+        VBox box = new VBox();
+        box.getChildren().addAll(txt, nameFld, btn);
+        root.getChildren().add(box);
         primaryStage.setTitle("Sup App");
         /*
         sets the scene height and width... just like the Canvas object in
